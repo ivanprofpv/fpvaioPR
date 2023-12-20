9dbb6068c59fb9aafecdfea32862a6b441958a08
ivanprofpv <akvabanka@gmail.com>
2023-12-20 15:49:15 +0400

    added button for start-menu

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -1,5 +1,20 @@
-from aiogram.types import Message
+from aiogram.types import Message, KeyboardButton, KeyboardButtonPollType
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 async def start(message: Message) -> None:
-    await message.answer('Привет')
+    menu_builder = ReplyKeyboardBuilder()
+    menu_builder.button(
+        text='Помощь'
+    )
+    menu_builder.add(
+        KeyboardButton(text='Отправить контакт', request_contact=True)
+    )
+    menu_builder.row(
+        KeyboardButton(text='Отправить голосование', request_poll=KeyboardButtonPollType())
+    )
+
+    await message.answer(
+        'Меню',
+        reply_markup=menu_builder.as_markup()
+    )
