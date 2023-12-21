ebe3ceb78a4fa19f1713a80b36ed36b237f4f0a4
ivanprofpv <akvabanka@gmail.com>
2023-12-21 14:47:49 +0400

    fix reg_date and upd_date for bd

--- bot/db/user.py
+++ bot/db/user.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import Column, Integer, VARCHAR, DATE
 
 from .base import BaseModel
@@ -12,10 +14,10 @@ class User(BaseModel):
     username = Column(VARCHAR(32), unique=False, nullable=True)
 
     # user registration date in bot
-    reg_date = Column()
+    reg_date = Column(DATE, default=datetime.date.today())
 
     # user last update date
-    upd_date = Column()
+    upd_date = Column(DATE, onupdate=datetime.date.today())
 
     def __str__(self) -> str:
         return f'<User: {self.user_id}'
