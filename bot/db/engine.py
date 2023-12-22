96e8242483b6c09a67eb0bc19e91b8a6d5e82937
ivanprofpv <akvabanka@gmail.com>
2023-12-22 13:18:24 +0400

    fix errors

--- bot/db/engine.py
+++ bot/db/engine.py
@@ -1,18 +1,31 @@
 from typing import Union
 
+import sqlalchemy
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import create_async_engine as _create_async_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 
-def create_async_engine(url: Union[URL, str]) -> AsyncEngine:
-    return _create_async_engine(url=url, echo=True, encodings='utf-8', pool_pre_ping=True)
+def create_async_engine(url: Union[URL, str]) -> sqlalchemy.ext.asyncio.AsyncEngine:
+    """
+
+    :param url:
+    :return:
+    """
+    return _create_async_engine(url=url, echo=True, pool_pre_ping=True)
 
 
-def proceed_schemas(engine: AsyncEngine, metadata) -> None:
-    with engine.connect() as conn:
-        conn.run_sync(metadata.create_all)
+async def proceed_schemas(engine: sqlalchemy.ext.asyncio.AsyncEngine, metadata: MetaData) -> None:
+    """
+
+    :param engine:
+    :param metadata:
+    """
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(metadata.create_all)
+    ...
 
 
 def get_session_maker(engine: AsyncEngine) -> sessionmaker:
